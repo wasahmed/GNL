b47f46fd243d28a6cfcd984cf466979e4591498c
Waseem AHMED <wasahmed@c4r10s3.wethinkcode.co.za>
2019-06-28 10:45:08 +0200

    gnl

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wasahmed <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/26 14:07:53 by wasahmed          #+#    #+#             */
+/*   Updated: 2019/06/28 10:30:46 by wasahmed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 100
+
+# include "fcntl.h"
+# include "sys/types.h"
+# include "sys/uio.h"
+# include "unistd.h"
+# include "stdlib.h"
+# include "libft/libft.h"
+
+int		ft_new_line(char **s, char **line, int fd, int ret);
+int		get_next_line(const int fd, char **line);
+
+#endif
---
+++ main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wasahmed <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/24 14:09:39 by wasahmed          #+#    #+#             */
+/*   Updated: 2019/06/27 17:24:01 by wasahmed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+
+	(void)argc;
+	fd = open(argv[1], O_RDONLY);
+	while (get_next_line(fd, &line) == 1)
+	{
+		printf("%s\n", line);
+		if (line)
+			ft_strdel(&line);
+	}
+	if (line)
+		free(line);
+	close(fd);
+	sleep(60);
+	return (0);
+}
